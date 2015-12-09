6ca14dc754b4f2a148d9f71bf888b2012259a8e9
wildfrancis <fduffy129@gmail.com>
2015-12-09 12:13:56 -0500

    enc/dec gui functional

---
+++ mainwindow.cpp
@@ -0,0 +1,121 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QFile>
+#include <QTextStream>
+#include <QString>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_encryptButton_clicked()
+{
+    setKey();
+    setTargetFile();
+    setDestFile();
+    encrypt(key, targetFile, destFile);
+}
+
+void MainWindow::on_decryptButton_clicked()
+{
+    setKey();
+    setTargetFile();
+    setDestFile();
+    decrypt(key, targetFile, destFile);
+}
+
+
+void MainWindow::encrypt(int key, QString targetFile, QString destFile)
+{
+    QFile inputFile(targetFile);
+    QFile outputFile(destFile);
+    inputFile.open(QIODevice::ReadWrite);
+    outputFile.open(QIODevice::ReadWrite);
+
+    qDebug() << targetFile << destFile;
+
+    QTextStream in(&inputFile);
+    QString text = in.readAll();
+
+    QTextStream out(&outputFile);
+
+    qDebug() << targetFile << destFile << text.size() << text;
+
+    int i=0;
+    int size = text.size();
+    while(i < size)
+    {
+        qDebug() << "after while loop ";
+        char val = text.at(i).toLatin1();
+
+        if (val >= 65 && val <= 122)
+            {
+                val = ((val + key) - 65) % 58 + 65;
+            }
+            out << val;
+            i++;
+    }
+
+    qDebug() << "function executed ";
+    inputFile.close();
+    outputFile.close();
+}
+void MainWindow::decrypt(int key, QString targetFile, QString destFile)
+{
+    QFile inputFile(targetFile), outputFile(destFile);
+    inputFile.open(QIODevice::ReadWrite);
+    outputFile.open(QIODevice::ReadWrite);
+
+    QTextStream in(&inputFile);
+    QString text = in.readAll();
+
+    QTextStream out(&outputFile);
+
+
+    for(int i=0; i < text.size(); i++)
+    {
+        char val = text.at(i).toLatin1();
+
+        if (val >= 65 && val <= 122)
+            {
+                val = (val - key - 65 + 58) % 58 + 65;
+            }
+            out << val;
+    }
+}
+
+void MainWindow::setTargetFile()
+{
+   targetFile = ui->targetFileLine->text();
+}
+
+void MainWindow::setDestFile()
+{
+    destFile = ui->destFileLine->text();
+}
+
+void MainWindow::setKey()
+{
+    key = ui->keyLine->text().toInt();
+}
+
+void MainWindow::printVals()
+{
+    qDebug() << key;
+    qDebug() << targetFile;
+    qDebug() << destFile;
+}
+
+void MainWindow::on_testButton_clicked()
+{
+    printVals();
+}
---
+++ mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QString>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void setTargetFile();
+    void setDestFile();
+    void setKey();
+    void printVals();
+
+
+private slots:
+    void on_encryptButton_clicked();
+
+    void on_decryptButton_clicked();
+
+    void on_testButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QString targetFile;
+    QString destFile;
+    int key;
+
+    void encrypt(int key, QString targetFile, QString destFile);
+    void decrypt(int key, QString targetFile, QString destFile);
+
+};
+
+#endif // MAINWINDOW_H
